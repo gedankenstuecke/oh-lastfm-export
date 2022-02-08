6d68a4b9e44176a623dc0eabf9bd66a3aba17be4
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2022-02-08 09:32:16 +0100

    add ping to heroku to wake up workers when running task

--- main/management/commands/update_jsons.py
+++ main/management/commands/update_jsons.py
@@ -3,12 +3,14 @@ from openhumans.models import OpenHumansMember
 from main import helpers
 from main.models import LastFmUser
 from main.tasks import last_fm_data
+import requests
 
 
 class Command(BaseCommand):
     help = 'Process so far unprocessed data sets'
 
     def handle(self, *args, **options):
+        requests.get('https://oh-lastfm-export.herokuapp.com/')
         #how to keep the heroku app alive despite a free dyno :joy:
         lastfm_users = LastFmUser.objects.all()
         for l in lastfm_users:
