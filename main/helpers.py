7f67497a10d55899a0c4938f0682e843e6b9eebf
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2022-02-15 17:39:40 +0100

    split data into years to avoid overflow

--- main/helpers.py
+++ main/helpers.py
@@ -2,12 +2,18 @@ import json
 import requests
 
 
-def get_existing_file(oh_member):
+def get_existing_file_latest(oh_member):
     files = oh_member.list_files()
+    lastfm_files = []
     for f in files:
-        if f['basename'] == 'lastfm-data.json':
-            data = json.loads(requests.get(f['download_url']).content)
-            return data
+        if f['basename'].startswith('lastfm-data'):
+            lastfm_files.append(f['basename'])
+    if len(lastfm_files) > 0:
+        target_file = sorted(lastfm_files)[-1]
+        for f in files:
+            if f['basename'] == target_file:
+                data = json.loads(requests.get(f['download_url']).content)
+                return data
     return []
 
 
@@ -15,3 +21,12 @@ def get_timestamp(data):
     for entry in data:
         if 'date' in entry.keys():
             return entry['date']['uts']
+
+
+def get_existing_year_file(oh_member, year):
+    files = oh_member.list_files()
+    for f in files:
+        if f['basename'] == ('lastfm-data-{}.json'.format(year)):
+            data = json.loads(requests.get(f['download_url']).content)
+            return data
+    return []
--- main/tasks.py
+++ main/tasks.py
@@ -6,7 +6,9 @@ import requests
 from openhumans.models import OpenHumansMember
 from celery import shared_task
 import os
-from .helpers import get_existing_file, get_timestamp
+from .helpers import get_existing_file_latest, get_timestamp
+from .helpers import get_existing_year_file
+import datetime
 
 LASTFM_URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key={}&format=json&limit=50".format(
     os.getenv('LASTFM_KEY')
@@ -19,7 +21,7 @@ def last_fm_data(oh_member_id):
     lastfm_user = oh_member.lastfmuser.username
     print('trying to update {}/{}'.format(lastfm_user, oh_member_id))
     user_url = LASTFM_URL + "&user={}".format(lastfm_user)
-    old_data = get_existing_file(oh_member)
+    old_data = get_existing_file_latest(oh_member)
     if old_data:
         from_timestamp = get_timestamp(old_data)
         user_url = user_url + "&from={}".format(from_timestamp)
@@ -34,25 +36,35 @@ def last_fm_data(oh_member_id):
     print(first_request.keys())
     tracks = first_request['recenttracks']['track']
     pages = first_request['recenttracks']['@attr']['totalPages']
-    # for i in range(2, 10):
+    # for i in range(2, 50):
     for i in range(2, int(pages)):
         print(i)
         time.sleep(1)
         p_url = user_url + "&page={}".format(i)
         page = requests.get(p_url).json()
         tracks = tracks + page['recenttracks']['track']
-    tracks = tracks + old_data
-    with tempfile.TemporaryFile() as f:
-        js = json.dumps(tracks)
-        js = str.encode(js)
-        f.write(js)
-        f.flush()
-        f.seek(0)
-        oh_member.delete_single_file(
-            file_basename='lastfm-data.json')
-        oh_member.upload(
-            f, "lastfm-data.json", metadata={
-                "description": "last.fm scrobbling history",
-                "tags": ["lastfm", 'scrobbles', 'music']
-                })
-        print('updated data for {}/{}'.format(lastfm_user, oh_member_id))
+    per_year = {}
+    for song in tracks:
+        if 'date' in song.keys():
+            year = datetime.datetime.fromtimestamp(int(song['date']['uts'])).year
+            if str(year) in per_year.keys():
+                per_year[str(year)].append(song)
+            else:
+                per_year[str(year)] = [song]
+    for year, songs in per_year.items():
+        old_data = get_existing_year_file(oh_member, year)
+        songs = songs + old_data
+        with tempfile.TemporaryFile() as f:
+            js = json.dumps(songs)
+            js = str.encode(js)
+            f.write(js)
+            f.flush()
+            f.seek(0)
+            oh_member.delete_single_file(
+                file_basename='lastfm-data-{}.json'.format(year))
+            oh_member.upload(
+                f, "lastfm-data-{}.json".format(year), metadata={
+                    "description": "last.fm scrobbling history",
+                    "tags": ["lastfm", 'scrobbles', 'music']
+                    })
+            print('updated data for {}/{}'.format(lastfm_user, year))
